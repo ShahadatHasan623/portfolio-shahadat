879daa90656c0a15b0a454238baea99d76463a28
ShahadatHasan623 <shahadathasan743599@gmail.com>
2025-12-18 21:38:32 +0600

    updated root elemant

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './Home/Home.jsx'
+
+
+const root=createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    children:[
+      {
+        path:'/',
+        element:<Home/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={root}/>
   </StrictMode>,
 )
